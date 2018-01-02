78db03cba79ce1b76d18e928aade17a000a9eb7b
Александр Витер <viter.alex@gmail.com>
2018-01-02 16:29:41 +0200

    Add project files.

---
+++ JaggedArrayControl/ArrayControl.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp1
+{
+    partial  class ArrayControl<TControl>
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countNud = new System.Windows.Forms.NumericUpDown();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.countNud)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // countNud
+            // 
+            this.countNud.Location = new System.Drawing.Point(3, 3);
+            this.countNud.Name = "countNud";
+            this.countNud.Size = new System.Drawing.Size(65, 20);
+            this.countNud.TabIndex = 0;
+            this.countNud.ValueChanged += new System.EventHandler(this.countNud_ValueChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.countNud);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(71, 26);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // ArrayControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "ArrayControl";
+            this.Size = new System.Drawing.Size(71, 26);
+            ((System.ComponentModel.ISupportInitialize)(this.countNud)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown countNud;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ JaggedArrayControl/ArrayControl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ArrayControl<TControl> : UserControl
+        where TControl: Control
+    {
+        public ArrayControl()
+        {
+            InitializeComponent();
+        }
+
+        protected Size DefaultElementSize = new Size(40, 20);
+
+        public virtual FlowDirection FlowDirection
+        {
+            get { return flowLayoutPanel1.FlowDirection; }
+            set { flowLayoutPanel1.FlowDirection = value; }
+        }
+
+        public string this[int i]
+        {
+            get { return flowLayoutPanel1.Controls.OfType<TControl>().FirstOrDefault(t => t.Name.Equals($"txt{i}"))?.Text; }
+        }
+
+
+        private void countNud_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateObjects();
+        }
+
+        private void UpdateObjects()
+        {
+            var fields = flowLayoutPanel1.Controls.OfType<TextBox>().ToList();
+            for (int i = fields.Count; i < countNud.Value; i++)
+            {
+                var ctrl = Activator.CreateInstance(typeof(TControl)) as Control;
+                ctrl.Name = $"txt{i}";
+                ctrl.Size = DefaultElementSize;
+                flowLayoutPanel1.Controls.Add(ctrl);
+            }
+
+            for (int i = fields.Count; i > countNud.Value; i--)
+            {
+                flowLayoutPanel1.Controls.RemoveAt(i);
+            }
+        }
+    }
+}
